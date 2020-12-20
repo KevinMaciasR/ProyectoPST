5094492736b04931d2c0da4b439d1258b35ff9ba
KevinMaciasR <wilter2607@gmail.com>
2020-12-20 14:29:41 -0500

    Inicio de la programacion

---
+++ sketch_dec20a/sketch_dec20a.ino
@@ -0,0 +1,20 @@
+/* Proyecto PST
+ *  Termometro a distancia con alarma
+*/
+// Librerias
+
+
+// Asignaciones
+
+
+// Asignacion de pines
+
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
